2990c478a10f3672f36e463effedbc9d66e46d3b
Usman Ahmed Saeed <umaan1982@gmail.com>
2025-03-26 22:26:54 +0100

    File system intro

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-fn main(){
+use std::io::prelude::*;
+use std::fs::File;
 
-    println!("Hello, world!");
+fn log_something(filename: &'static str, string: &'static [u8; 17]) -> Result<File, std::io::Error> {
+    let mut f = File::create(filename).unwrap();
+    f.write_all(string)?;
+    Ok(f)
+}
+
+fn main() {
+    match log_something("hello.txt", b"Trying out things") {
+        Ok(..) => println!("File created!"),
+        Err(..) => println!("Error: could not create file.")
+    }
 }
