b30faf43ee6c9c5af4570416793cde668e04cf8f
Usman Ahmed Saeed <umaan1982@gmail.com>
2025-03-26 23:39:12 +0100

    Enabled web app server, returned timestamps

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+
+#[macro_use] extern crate nickel;
 extern crate chrono;
 
 use std::io::prelude::*;
+use tokio; 
 use std::fs::{OpenOptions};
 use std::io;
 use chrono::*;
+use nickel::Nickel;
 
 fn formatted_time_entry() -> String {
     let local: DateTime<Local> = Local::now();
@@ -21,17 +25,38 @@ fn record_entry_in_log(filename: &str, bytes: &[u8]) -> io::Result<()> {
     Ok(())
 }
 
-fn log_time(filename: &'static str) -> io::Result<()> {
+fn log_time(filename: &'static str) -> io::Result<String> {
     let entry = formatted_time_entry();
     let bytes = entry.as_bytes();
 
     (record_entry_in_log(filename, &bytes))?;
-    Ok(())
+    Ok(entry)
 }
 
-fn main() {
+fn do_log_time() -> String {
     match log_time("log.txt") {
-        Ok(..) => println!("File created!"),
-        Err(..) => println!("Error: could not create file.")
+        Ok(entry) => format!("File created! {}", entry),
+        Err(e) => format!("Error: {}", e)
     }
+}
+
+async fn listen_server(server: Nickel) {
+    server.listen("127.0.0.1:6767").await.expect("Failed to start server");
+}
+
+
+fn main() {
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            do_log_time()
+        }
+    });
+
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(listen_server(server));
 }
